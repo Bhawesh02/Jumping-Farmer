491c12cf0e9c23a94bf9950b94fe5805900e329d
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-09-03 14:37:11 +0530

    Creater Boulder prefab and made it move left

---
+++ Jumping Farmer/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody playerRb ;
+    public float jumpForce = 13;
+    public float gravityModifier = 2.0f;
+    public bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
